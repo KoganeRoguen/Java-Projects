2ec91bdcb3615f3e3e21b57349ba6686f6fdf25c
KoganeRoguen <costa.farias@escolar.ifrn.edu.br>
2026-08-17 20:19:37 -0300

    Add files via upload

---
+++ MediaEstudante.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MediaEstudante {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("> Informe a primeira nota: _");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("> Informe a segunda nota: _");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("> Informe a terceira nota: _");
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.printf("A média aritimética é: %.2f\n", media);
+
+        scanner.close();
+    }
+}
