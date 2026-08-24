b790a575ce64bc150995284b3a86afdd5762063b
KoganeRoguen <costa.farias@escolar.ifrn.edu.br>
2026-08-24 15:37:46 -0300

    Add files via upload

---
+++ Práticas 2/soma.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+
+public class soma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printint("> Informe o primeiro valor: _");
+        double x = Scanner.nextDouble
+        System.out.printint("> Informe o segundo valor: _");
+        double y = Scanner.nextDouble
+
+        double soma = x + y
+        
+        System.out.printf("\n> A soma dos valores ", x, " e ", y, " é: ", soma);
+        Scanner.close();
+    }
+}
