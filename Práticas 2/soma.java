53b1879205bed7042354c45d99c17fa6463bf743
KoganeRoguen <costa.farias@escolar.ifrn.edu.br>
2026-08-24 15:42:08 -0300

    Fix syntax errors in soma.java

--- Práticas 2/soma.java
+++ Práticas 2/soma.java
@@ -1,16 +1,19 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class soma {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printint("> Informe o primeiro valor: _");
-        double x = Scanner.nextDouble
-        System.out.printint("> Informe o segundo valor: _");
-        double y = Scanner.nextDouble
 
-        double soma = x + y
+        System.out.print("> Informe o primeiro valor: _");
+        double x = scanner.nextDouble();
+
+        System.out.print("> Informe o segundo valor: _");
+        double y = scanner.nextDouble();
+
+        double soma = x + y;
         
-        System.out.printf("\n> A soma dos valores ", x, " e ", y, " é: ", soma);
-        Scanner.close();
+        System.out.printf("\n> A soma dos valores " + x + " e " + y + " é: " + soma + "\n\n");
+        
+        scanner.close();
     }
 }
