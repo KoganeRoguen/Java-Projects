2eea9b2cf09a95da78b9192aabfa7447c569ef2d
KoganeRoguen <costa.farias@escolar.ifrn.edu.br>
2026-08-24 15:45:17 -0300

    Modify user greeting and input prompt in hellouser
    Updated greeting message and prompt for user input.

--- Práticas 1/hellouser.java
+++ Práticas 1/hellouser.java
@@ -4,10 +4,10 @@ public class hellouser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print(">_ ");
+        System.out.print("> Informe o seu nome _");
         String nome = scanner.nextLine();
 
-        System.out.println("Olá, " + nome + "! Seja bem-vindo(a)!");
+        System.out.println("\nOlá, " + nome + "! Seja bem-vindo(a)!\n");
 
         scanner.close();
     }
